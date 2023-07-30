d497118a50695befddbbae8b2f5c5334eb410c00
massa <zoumaniguimassa12@gmail.com>
2023-07-30 19:32:51 +0000

    setup

---
+++ PopupApp.js
@@ -0,0 +1,83 @@
+const projects = [
+ {
+   title: "ShowMovie",
+   description: "A ShowMovie Site ",
+   imageSrc: "./images/reactmovie.png",
+   liveDemoLink: "https://zoumshowmovie.onrender.com",
+   codeLink: "https://github.com/examplehttps://zoumshowmovie.onrender.com"
+ },
+ {
+   title: "NationConnect",
+   description: "This is my third project",
+   imageSrc: "./images/nation.png",
+   liveDemoLink: "https://space-x-27k2.onrender.com",
+   codeLink: "https://github.com/massazoum/NationsConnect"
+ },
+ {
+  title: "Bookstore",
+  description: "This bookstore helps us to add and to remove book from API",
+  description1:"The project is a user-friendly and efficient online bookstore application that allows users to easily manage the inventory of books stored in an external API. The primary objective is to simplify the process of adding new books to the API and removing existing ones.With this application, users can conveniently add book details such as title, author, genre, and publication information, and seamlessly upload them to the API. The interface is designed to be intuitive and straightforward, enabling users to input the necessary information with ease.",
+  imageSrc: "./images/Bookstore.png",
+  liveDemoLink: "https://example.com",
+  codeLink: "https://github.com/example"
+},
+{
+  title: "Space",
+  description: "This is my second project",
+  imageSrc: "./images/space.png",
+  liveDemoLink: "https://example.com",
+  codeLink: "https://github.com/Shaheryar0054/react-group-project"
+},
+{
+  title: "Math_magicians",
+  description: 'Math_magicians is a comprehensive calculator',
+  description1: "Math_magicians is a comprehensive calculator project designed to provide users with a wide range of mathematical functionalities and capabilities. This project aims to create a versatile and user-friendly calculator application that offers a seamless experience for performing various mathematical calculations.Math_magicians incorporates a sleek and intuitive user interface, allowing users to input mathematical expressions and receive accurate results promptly.",
+  imageSrc: "./images/Math_magicians.png",
+  liveDemoLink: "https://math-magicians-ue5p.onrender.com",
+  codeLink: "https://github.com/massazoum/Math-magicians"
+}
+,{
+  title: "Project 2",
+  description: "This capstone projet is around the artificial intelligence in order to improve our nation education",
+  description1:'',
+  imageSrc: "./images/AI.png",
+  liveDemoLink: "https://massazoum.github.io/capstone-project-1",
+  codeLink: "https://github.com/massazoum/capstone-project-1"
+},
+];
+
+// Render projects
+const projectsContainer = document.querySelector(".projects");
+projects.forEach(project => {
+ const card = document.createElement("div");
+ card.className = "card";
+ card.dataset.aos = 'zoom-in';
+ card.innerHTML = `
+ <img src="${project.imageSrc}" class="ImageCard" alt="${project.title}" width="100%" onclick="openCardPopup('${project.title}', '${project.description}', '${project.imageSrc}', '${project.liveDemoLink}', '${project.codeLink}')">
+ <div class="color">${project.title}</div>
+ <div class="color">${project.description}</div>
+ <div  class="color"><span type="button" disabled>HTML</span><span type="button" disabled>CSS</span><span type="button" disabled>JavaScript</span></div>
+ <button class="see-project-button" onclick="openCardPopup('${project.title}', '${project.description1}', '${project.imageSrc}', '${project.liveDemoLink}', '${project.codeLink}')">See Project</button>
+
+ `;
+ projectsContainer.appendChild(card);
+});
+
+// Card Popup
+function openCardPopup(title, description, imageSrc, liveDemoLink, codeLink) {
+ document.getElementById("cardPopupTitle").textContent = title;
+ document.getElementById("cardPopupDescription").textContent = description;
+ document.getElementById("cardPopupImage").src = imageSrc;
+ document.getElementById("cardPopup").classList.add("active");
+ document.getElementById("cardPopupLiveDemo").href = liveDemoLink;
+ document.getElementById("cardPopupCode").href= codeLink;
+}
+
+function closeCardPopup() {
+ document.getElementById("cardPopup").classList.remove("active");
+}
+
+// Form Submission
+function submitForm(event) {
+ event.preventDefault();
+}
---
+++ script.js
@@ -0,0 +1,49 @@
+const toggle = document.querySelector('.toggle');
+const body = document.querySelector('body');
+const menuOptions = document.querySelectorAll('.menu li');
+
+toggle.addEventListener('click', () => {
+  body.classList.toggle('open');
+});
+
+menuOptions.forEach((option) => {
+  option.addEventListener('click', () => {
+    body.classList.remove('open');
+  });
+});
+
+let typed =new Typed(".typing",{
+ strings:['Full-stack developer','Remote professional','Micronaute','Traveler'],
+ typeSpeed:100,
+ backSpeed:60,
+ loop:true,
+})
+
+// get form elements
+const form = document.querySelector('.form0');
+const emailInput = form.querySelector('.inp-email');
+const submitBtn = form.querySelector('.submit0');
+
+// add event listener to the form submission
+form.addEventListener('submit', (event) => {
+  // prevent default form submission behavior
+  event.preventDefault();
+
+  // check if email is in lowercase
+  if (emailInput.value === emailInput.value.toLowerCase()) {
+    // email is in lowercase, submit the form
+    form.submit();
+  } else {
+    // email is not in lowercase, show error message
+    const errorMsg = document.querySelector('.error-message');
+    errorMsg.textContent = 'Email should be in lowercase.';
+    errorMsg.style.color = '#FF4136';
+    submitBtn.insertAdjacentElement('afterend', errorMsg);
+  }
+});
+
+
+AOS.init({
+  duration: 1000,
+  once: false,
+});
